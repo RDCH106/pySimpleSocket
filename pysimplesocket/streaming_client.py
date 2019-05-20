016c2633a85c7e055842274f6168e9f43410b030
rdecelis <rdecelis@stt-systems.com>
2019-05-20 15:01:25 +0200

    Refactorize streaming_client.py

--- pysimplesocket/streaming_client.py
+++ pysimplesocket/streaming_client.py
@@ -1,42 +1,39 @@
 # -*- coding: utf-8 -*-
 
 import socket
-import threading
 import time
 import sys
 import argparse
+try:
+    from pysimplesocket.abstract_client import AbstractClient, Metadata
+except ModuleNotFoundError as e:
+    from abstract_client import AbstractClient, Metadata
 
 
-class Metadata:
-    def __init__(self):
-        self.__version__ = '0.0.1'
-        self.__author__ = 'Rubén de Celis Hernández'
+class StreamingClient(AbstractClient):
 
-    def get_version(self):
-        return self.__version__
-
-    def get_author(self):
-        return self.__author__
-
-
-class StreamingClient(object):
+    meta = Metadata(version='0.0.2', author='Rubén de Celis Hernández')
 
     def __init__(self, ip=None, port=None):
         self.__ip = ip
         self.__port = port
-        self.__receiver_thread = None
-        self.__stop = False
+        # self.__receiver_thread = None
+        # self.__stop = False
+        self.__socket_family = socket.AF_INET
+        self.__socket_type = socket.SOCK_STREAM
+        super(StreamingClient, self).__init__()
 
     def receiver_worker(self):
         try:
             # create an ipv4 (AF_INET) socket object using the tcp protocol (SOCK_STREAM)
-            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client = socket.socket(self.__socket_family, self.__socket_type)
 
             # connect the client
             client.connect((self.__ip, self.__port))
             print('Socket opened')
 
-            while not self.__stop:
+            # while not self.__stop:
+            while not self.v_stop:
                 # receive the response data (4096 is recommended buffer size)
                 response = client.recv(4096)
 
@@ -54,24 +51,27 @@ class StreamingClient(object):
             print('Socket closed')
 
     def run(self):
-        if self.__receiver_thread is None:
-            self.__stop = False
-
-            self.__receiver_thread = threading.Thread(target=self.receiver_worker)
-            self.__receiver_thread.daemon = True
-            self.__receiver_thread.start()
+        super(StreamingClient, self).run()
+        # if self.__receiver_thread is None:
+        #     self.__stop = False
+        #
+        #     self.__receiver_thread = threading.Thread(target=self.receiver_worker)
+        #     self.__receiver_thread.daemon = True
+        #     self.__receiver_thread.start()
 
     def stop(self):
-        self.__receiver_thread = None
-        self.__stop = True
+        super(StreamingClient, self).stop()
+        # self.__receiver_thread = None
+        # self.__stop = True
 
 
 if __name__ == "__main__":
 
     # Parse arguments provided
     parser = argparse.ArgumentParser()
-    meta = Metadata()
-    parser.add_argument('-v', '--version', action='version', version=meta.get_version())
+    # meta = Metadata()
+    # parser.add_argument('-v', '--version', action='version', version=meta.get_version())
+    parser.add_argument('-v', '--version', action='version', version=StreamingClient.meta.get_version())
     parser.add_argument('-s', '--host', dest='host', help='Host server to connect', default=None, required=True)
     parser.add_argument('-p', '--port', dest='port', help='Port to connect', default=None, required=True)
 
